ede10881a45eab47bce4e65165c8a3a0169ac475
Cracktus Heinkel <78656905+Cracko298@users.noreply.github.com>
2021-08-14 13:54:34 -0400

    Added new source for Firmware 11.15-47U/E

---
+++ Sources/cheats.h
@@ -0,0 +1,19 @@
+#ifndef CHEATS_H
+#define CHEATS_H
+
+#include "plugin.h"
+
+void    Rainbow_Clothes(void);
+void	Grey_Clothes(void);
+void	Brown_Clothes(void);
+void	Black_Clothes(void);
+void	Blue_Clothes(void);
+void	Grey_and_Black(void);
+void	Tan_Clothes(void);
+void	Red_Clothes(void);
+void	Bling_Clothes(void);
+void	Kvlar_Clothes(void);
+void	Default_Clothes(void);
+void	Arctic_Clothes(void);
+
+#endif
